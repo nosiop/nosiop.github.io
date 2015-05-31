672c5a562e8406e91edcfa491b581f8fbe3b8d32
nosiop <poison11@163.com>
2015-06-01 01:46:10 +0800

    add backToTop

--- js/kapp.js
+++ js/kapp.js
@@ -3,7 +3,7 @@
 
 $(document).ready(function() {
 	generateIndex();
-//	backToTop();
+	backToTop();
 
 });
 
@@ -14,6 +14,25 @@ function generateIndex() {
 		$('#markdown-toc').hide();
 		$('#side_index').html('<h1>文章索引</h1><br>'+'<ul>'+$('#markdown-toc').html()+'</ul>');
 	}
+}
+
+function backToTop() {
+	// 滚动页面才显示回到顶部
+	$(window).scroll(function(){
+		if($(window).scrollTop() > 100) { 
+			$('#backTop').fadeIn(500);
+		} else {
+			$('#backTop').fadeOut(500);		
+		}
+	});
+	// 点击回到顶部
+	$('#backTop').click(function() {
+		$('body').animate({scrollTop:"0"},500);
+	});
+	//初始化
+	$(function() {
+		$('[data-toggle="tooltip"]').tooltip();
+	});
 }
 
 
