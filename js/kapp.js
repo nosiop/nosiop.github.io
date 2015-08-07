7dea4b89146c1a64e9cdf0dbb3a8aca167179fd5
nosiop <poison11@163.com>
2015-08-07 16:05:59 +0800

    design change to fit different devices

--- js/kapp.js
+++ js/kapp.js
@@ -4,7 +4,40 @@
 $(document).ready(function() {
 	generateIndex();
 	backToTop();
+	resizeWindow();
 });
+
+function resizeWindow(){
+
+	var win_width = $(window).width();
+	var mleft = win_width/10; 
+	$('.well_list').css('margin-left', mleft);
+	if( win_width <= 1000){
+		$('.well_sidebar').hide();
+		$('.sidebar').hide();
+		$('.col-md-9').css('width','100%');
+	} else {
+		$('.well_sidebar').show();
+		$('.sidebar').show();
+		$('.col-md-9').css('width','70%');
+	}
+
+
+    $(window).resize(function() { //when resize window, hide sidebar and adjust main column
+		var win_width = $(window).width();
+		var mleft = win_width/10; 
+		$('.well_list').css('margin-left', mleft);
+		if(win_width <= 1000){
+			$('.well_sidebar').hide();
+			$('.sidebar').hide();
+			$('.col-md-9').css('width','100%');
+		} else {
+			$('.well_sidebar').show();
+			$('.sidebar').show();
+			$('.col-md-9').css('width','70%');
+		}
+    });
+}
 
 function generateIndex(){
 	if(typeof $('#markdown-toc').html() === 'undefined'){
